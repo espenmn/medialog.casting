12e7eaa27ecf7382c3ab598464f02ba225bc2730
Espen Moe-Nilssen <espen@medialog.no>
2019-06-11 12:43:55 +0200

    Adding 'back references'

--- src/medialog/casting/browser/views.py
+++ src/medialog/casting/browser/views.py
@@ -2,7 +2,12 @@
 from Products.Five import BrowserView
 #from plone import api
 from datetime import date
-
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from collections import OrderedDict
 
 class IActorView(BrowserView):
     """collection view"""
@@ -17,9 +22,44 @@ class IActorView(BrowserView):
         days_in_year = 365.2425
         return int((date.today() - self.context.born).days / days_in_year)
 
+    def get_relateditems(self):
+        refs = (self.context.relatedItems)
+        to_objects = [ref.to_object for ref in refs]
+        refers = self.get_referers(self.context)
+        from_objects = [ref.from_object for ref in refers]
+        ref_list = to_objects + from_objects
+        return OrderedDict( (x,1) for x in ref_list ).keys()
+        #return ref_list
 
+    def get_referers(self, context = None):
+        """ Return a list of backreference relationvalues
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+        context = context and context or self.context
+        rel_query = { 'to_id' : intids.getId(aq_inner(context)) }
+        rel_items = list(catalog.findRelations(rel_query))
+        return rel_items
+
+    # def get_brains_for_relation_ids(context, relationvalues, sort_key=None, direction='from', depth=0, portal_type=[], language='', keep_original_order=None):
+    #     p_catalog = getToolByName(context, 'portal_catalog')
+    #     results = []
+    #     query = {}
+    #
+    #     if portal_type:
+    #         query['Type'] = portal_type
+    #
+    #     #no multilanguage in this site
+    #     #if language:
+    #     #    query['Language'] = language
+    #
+    #     #log.info('%s Relationvalues ids: %s' % (get_linenumber(), [relationvalue.to_path.split('/')[-1] for relationvalue in relationvalues]))
+    #
+    #     for relationvalue in relationvalues:
+    #         r_path = direction == 'from' and relationvalue.from_path or relationvalue.to_path
+    #
+    #         for search_depth in range(0, depth+1):
+    #             query['path'] = {'query': r_path, 'depth': search_depth}
 
 class FolderView(BrowserView):
     pass
-
-    
