e0962827ab739b130965cf337e611e7639b5a3ca
Espen Moe-Nilssen <espen@medialog.no>
2019-06-13 16:18:16 +0200

    typo

--- src/medialog/casting/behaviors.py
+++ src/medialog/casting/behaviors.py
@@ -181,7 +181,7 @@ class IRelatedActorBehavior(model.Schema):
         default=[],
         value_type=RelationChoice(
             title=u'Personer',
-            source=CatalogSource(portal_type='Person', 'person'),
+            source=CatalogSource(portal_type=['Person', 'person']),
         ),
         required=False,
     )
